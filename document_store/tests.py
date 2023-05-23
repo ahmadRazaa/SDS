29f91a2132d68acf567b8bd5068ff0381b89465a
Ahmad Raza <ahmad24013@gmail.com>
2023-05-23 20:24:22 +0500

    updated test cases for authentication

--- document_store/tests.py
+++ document_store/tests.py
@@ -1,18 +1,27 @@
+from django.contrib.auth.models import User
 from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
+from .cache_manager import CacheKeys
 from .models import Document, Folder, Topic
 from .pagination import StandardPagination
 from .serializers import DocumentSerializer, FolderSerializer, TopicSerializer
-from .cache_manager import CacheKeys
 
 
 class TopicViewSetTestCase(TestCase):
     def setUp(self):
+        self.client = APIClient()
+        # Create a user and obtain an authentication token
+        self.user = User.objects.create_user(
+            username="test_user", password="test_password"
+        )
+        self.client.force_authenticate(user=self.user)
+
         self.topic1 = Topic.objects.create(name="Topic 1")
         self.topic2 = Topic.objects.create(name="Topic 2")
         self.valid_payload = {"name": "New Topic"}
@@ -23,24 +32,21 @@ class TopicViewSetTestCase(TestCase):
         cache.clear()
 
     def test_create_topic_with_valid_payload(self):
-        client = APIClient()
         url = reverse("topic-list")
-        response = client.post(url, data=self.valid_payload)
+        response = self.client.post(url, data=self.valid_payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Topic.objects.count(), 3)
 
     def test_create_topic_with_invalid_payload(self):
-        client = APIClient()
         url = reverse("topic-list")
-        response = client.post(url, data=self.invalid_payload)
+        response = self.client.post(url, data=self.invalid_payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Topic.objects.count(), 2)
 
     def test_get_topics(self):
-        client = APIClient()
-        response = client.get(reverse("topic-list"))
+        response = self.client.get(reverse("topic-list"))
 
         response.query_params = {}
         paginator = StandardPagination()
@@ -52,9 +58,8 @@ class TopicViewSetTestCase(TestCase):
         self.assertEqual(response.data.get("results"), serializer.data)
 
     def test_get_topic(self):
-        client = APIClient()
         url = reverse("topic-detail", args=[self.topic1.pk])
-        response = client.get(url)
+        response = self.client.get(url)
 
         topic = Topic.objects.get(pk=self.topic1.pk)
         serializer = TopicSerializer(topic)
@@ -63,16 +68,15 @@ class TopicViewSetTestCase(TestCase):
         self.assertEqual(response.data, serializer.data)
 
     def test_topic_list_caching(self):
-        client = APIClient()
         # Make the first request
-        response1 = client.get(reverse("topic-list"))
+        response1 = self.client.get(reverse("topic-list"))
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
         self.assertEqual(response1.data, cache.get(CacheKeys.TOPIC_LIST_KEY))
 
         # Update the topic and make a second request
         self.topic1.name = "Updated Topic"
         self.topic1.save()
-        response2 = client.get(reverse("topic-list"))
+        response2 = self.client.get(reverse("topic-list"))
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
         self.assertEqual(response2.data, cache.get(CacheKeys.TOPIC_LIST_KEY))
 
@@ -103,6 +107,13 @@ class TopicViewSetTestCase(TestCase):
 
 class FolderViewSetTestCase(TestCase):
     def setUp(self):
+        self.client = APIClient()
+        # Create a user and obtain an authentication token
+        self.user = User.objects.create_user(
+            username="test_user", password="test_password"
+        )
+        self.client.force_authenticate(user=self.user)
+
         self.folder1 = Folder.objects.create(name="Folder 1")
         self.folder2 = Folder.objects.create(name="Folder 2")
         self.valid_payload = {"name": "New Folder"}
@@ -113,26 +124,23 @@ class FolderViewSetTestCase(TestCase):
         cache.clear()
 
     def test_create_folder_with_valid_payload(self):
-        client = APIClient()
         url = reverse("folder-list")
 
-        response = client.post(url, data=self.valid_payload)
+        response = self.client.post(url, data=self.valid_payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Folder.objects.count(), 3)
 
     def test_create_folder_with_invalid_payload(self):
-        client = APIClient()
         url = reverse("folder-list")
 
-        response = client.post(url, data=self.invalid_payload)
+        response = self.client.post(url, data=self.invalid_payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Folder.objects.count(), 2)
 
     def test_get_folders(self):
-        client = APIClient()
-        response = client.get(reverse("folder-list"))
+        response = self.client.get(reverse("folder-list"))
 
         folders = Folder.objects.all()
         response.query_params = {}
@@ -144,9 +152,8 @@ class FolderViewSetTestCase(TestCase):
         self.assertEqual(response.data.get("results"), serializer.data)
 
     def test_get_folder(self):
-        client = APIClient()
         url = reverse("folder-detail", args=[self.folder1.pk])
-        response = client.get(url)
+        response = self.client.get(url)
 
         folder = Folder.objects.get(pk=self.folder1.pk)
         serializer = FolderSerializer(folder)
@@ -155,16 +162,15 @@ class FolderViewSetTestCase(TestCase):
         self.assertEqual(response.data, serializer.data)
 
     def test_folder_list_caching(self):
-        client = APIClient()
         # Make the first request
-        response1 = client.get(reverse("folder-list"))
+        response1 = self.client.get(reverse("folder-list"))
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
         self.assertEqual(response1.data, cache.get(CacheKeys.FOLDER_LIST_KEY))
 
         # Update the folder and make a second request
         self.folder1.name = "Updated Folder"
         self.folder1.save()
-        response2 = client.get(reverse("folder-list"))
+        response2 = self.client.get(reverse("folder-list"))
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
         self.assertEqual(response2.data, cache.get(CacheKeys.FOLDER_LIST_KEY))
 
@@ -173,6 +179,13 @@ class FolderViewSetTestCase(TestCase):
 
 class DocumentViewSetTestCase(TestCase):
     def setUp(self):
+        self.client = APIClient()
+        # Create a user and obtain an authentication token
+        self.user = User.objects.create_user(
+            username="test_user", password="test_password"
+        )
+        self.client.force_authenticate(user=self.user)
+
         self.topic = Topic.objects.create(name="Topic")
         self.folder = Folder.objects.create(name="Folder")
         self.document1 = Document.objects.create(
@@ -194,24 +207,21 @@ class DocumentViewSetTestCase(TestCase):
         cache.clear()
 
     def test_create_document_with_valid_payload(self):
-        client = APIClient()
         url = reverse("document-list")
-        response = client.post(url, data=self.valid_payload)
+        response = self.client.post(url, data=self.valid_payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Document.objects.count(), 3)
 
     def test_create_document_with_invalid_payload(self):
-        client = APIClient()
         url = reverse("document-list")
-        response = client.post(url, data=self.invalid_payload)
+        response = self.client.post(url, data=self.invalid_payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Document.objects.count(), 2)
 
     def test_get_documents(self):
-        client = APIClient()
-        response = client.get(reverse("document-list"))
+        response = self.client.get(reverse("document-list"))
 
         documents = Document.objects.all()
         response.query_params = {}
@@ -223,10 +233,9 @@ class DocumentViewSetTestCase(TestCase):
         self.assertEqual(response.data.get("results"), serializer.data)
 
     def test_filter_documents_by_topic(self):
-        client = APIClient()
         url = reverse("document-list")
         topic_name = "Topic"
-        response = client.get(url, {"topic": topic_name})
+        response = self.client.get(url, {"topic": topic_name})
 
         documents = Document.objects.filter(topic__name=topic_name)
         response.query_params = {}
@@ -238,9 +247,8 @@ class DocumentViewSetTestCase(TestCase):
         self.assertEqual(response.data.get("results"), serializer.data)
 
     def test_get_document(self):
-        client = APIClient()
         url = reverse("document-detail", args=[self.document1.pk])
-        response = client.get(url)
+        response = self.client.get(url)
 
         document = Document.objects.get(pk=self.document1.pk)
         serializer = DocumentSerializer(document)
@@ -249,27 +257,24 @@ class DocumentViewSetTestCase(TestCase):
         self.assertEqual(response.data, serializer.data)
 
     def test_document_list_caching(self):
-        client = APIClient()
         # Make the first request
-        response1 = client.get(reverse("document-list"))
+        response1 = self.client.get(reverse("document-list"))
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
         self.assertEqual(response1.data, cache.get(CacheKeys.DOCUMENT_LIST_KEY))
 
         # Update the doc and make a second request
         self.document1.name = "Updated document"
         self.document1.save()
-        response2 = client.get(reverse("document-list"))
+        response2 = self.client.get(reverse("document-list"))
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
         self.assertEqual(response2.data, cache.get(CacheKeys.DOCUMENT_LIST_KEY))
 
         self.assertNotEqual(response1.data, response2.data)
 
     def test_document_detail_caching(self):
-        client = APIClient()
-
         # Make the first request
         url = reverse("document-detail", args=[self.document1.pk])
-        response1 = client.get(url)
+        response1 = self.client.get(url)
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
         self.assertEqual(
             response1.data,
@@ -279,7 +284,7 @@ class DocumentViewSetTestCase(TestCase):
         # Update the document and make a second request
         self.document1.name = "Updated Document"
         self.document1.save()
-        response2 = client.get(url)
+        response2 = self.client.get(url)
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
         self.assertEqual(
             response2.data,
