0e8d80663c668eafd4a90739429bcfc52ac234f5
Ahmad Raza <ahmad24013@gmail.com>
2023-05-23 18:57:37 +0500

    added TopicViewSet caching

--- document_store/views.py
+++ document_store/views.py
@@ -1,5 +1,10 @@
+from django.core.cache import cache
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from rest_framework import viewsets
+from rest_framework.response import Response
 
+from .cache_manager import CacheKeys
 from .models import Document, Folder, Topic
 from .pagination import StandardPagination
 from .serializers import FolderSerializer, TopicSerializer, DocumentSerializer
@@ -10,6 +15,27 @@ class TopicViewSet(viewsets.ModelViewSet):
     serializer_class = TopicSerializer
     pagination_class = StandardPagination
     permission_classes = []
+
+    def list(self, request, *args, **kwargs):
+        cached_data = cache.get(CacheKeys.TOPIC_LIST_KEY)
+
+        if cached_data:
+            return Response(cached_data)
+
+        response = super().list(request, *args, **kwargs)
+        cache.set(CacheKeys.TOPIC_LIST_KEY, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = CacheKeys.TOPIC_DETAIL_KEY_PREFIX + kwargs["pk"]
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+
+        response = super().retrieve(request, *args, **kwargs)
+        cache.set(cache_key, response.data)
+        return response
 
 
 class FolderViewSet(viewsets.ModelViewSet):
@@ -32,3 +58,15 @@ class DocumentViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(topic__name=topic)
 
         return queryset
+
+
+@receiver([post_save, post_delete], sender=Topic)
+@receiver([post_save, post_delete], sender=Folder)
+@receiver([post_save, post_delete], sender=Document)
+def invalidate_cache(sender, instance, **kwargs):
+    cache.delete(CacheKeys.TOPIC_LIST_KEY)
+    cache.delete(CacheKeys.TOPIC_DETAIL_KEY_PREFIX + str(instance.pk))
+    cache.delete(CacheKeys.FOLDER_LIST_KEY)
+    cache.delete(CacheKeys.FOLDER_DETAIL_KEY_PREFIX + str(instance.pk))
+    cache.delete(CacheKeys.DOCUMENT_LIST_KEY)
+    cache.delete(CacheKeys.DOCUMENT_DETAIL_KEY_PREFIX + str(instance.pk))
